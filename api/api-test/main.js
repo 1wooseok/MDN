de69ad8c7dd66e0585031529239bdb31881bddac
1wooseok <74036731+1wooseok@users.noreply.github.com>
2021-09-04 20:29:13 +0900

    21-09-04

--- api/api-test/main.js
+++ api/api-test/main.js
@@ -1,4 +1,4 @@
-const h2 = document.querySelector('h2');
+const h2 = document.querySelector('.cityName');
 const para = document.querySelector('p');
 const search = document.querySelector('.search');
 const search_btn = document.querySelector('.search_btn');
@@ -9,6 +9,7 @@ const base_url = 'http://api.data.go.kr/openapi/tn_pubr_public_weighted_envlp_ap
 const api_key = 'V9hsM%2B3pd0KWaNCIJ82cLDqU1hJVBsaHI%2FnOwxsK1utzoE3d7HCHPYmKhD4zyUfoz2izB%2F9FiDJZFs6Hlt6b0g%3D%3D';
 let url;
 
+// 공공데이터 fetch
 function fetchResult(evt) {
   url = `${base_url}?ServiceKey=${api_key}`;
 
@@ -19,6 +20,7 @@ function fetchResult(evt) {
      })
 }
 
+// 공공데이터 Str -> XML(object)
 function strToXML(xml) {
   const test1 = {
     age: 11,
@@ -30,25 +32,56 @@ function strToXML(xml) {
   myXML = myParser.parseFromString(myString, "text/xml");
 }
 
+// 시군구 검색
 function searchCity() {
   let searched_city = search.value;
   search.value = '';
 
-  displayCityName(searched_city);
+  displayCity(searched_city);
 }
 
-function displayCityName(searched_city) {
+// 종량제 봉투 정보 화면에 출력
+function displayCity(searched_city) {
+  console.log(myXML);
+  
   let item = myXML.getElementsByTagName('item');
 
+  // nextNode 이런걸로 가격 뽑기
   for(let i=0; i<item.length; i++) {
     let city = item[i].getElementsByTagName('signguNm')[0].textContent;
-    if(String(searched_city) === String(city)) {
+    if(searched_city === city) {
       h2.textContent = city;
+
+      let price = item[0].getElementsByTagName('price1')[0];
+
+      //let k = 0;
+      while(price.nodeName != 'chrgDeptNm') {
+        const para2 = document.createElement('p');
+        const span1 = document.createElement('span');
+        const span2 = document.createElement('span');
+
+        let myPrice = (price.nodeName).replace('price', '') + 'ℓ : ';
+
+        span1.textContent = myPrice;
+        span2.textContent = price.textContent + '원';
+
+        if(price.textContent != 0) {
+          para2.style.backgroundColor = 'tomato';
+        }
+
+        para2.appendChild(span1);
+        para2.appendChild(span2);
+
+        para.appendChild(para2);
+
+        price = price.nextSibling;
+      }
       return
-    } 
+    }
   }
   h2.textContent = '옳바른 도시이름을 입력 해주세요.';
 }
+
 
 function init() {
   fetchResult();
