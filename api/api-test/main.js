df8654a69d3aab7098014be5beaecd382a9d2e33
1wooseok <74036731+1wooseok@users.noreply.github.com>
2021-09-08 00:19:21 +0900

    21-09-08

--- api/api-test/main.js
+++ api/api-test/main.js
@@ -1,21 +1,20 @@
 const h2 = document.querySelector('.cityName');
 const para = document.querySelector('p');
-const search = document.querySelector('.search');
+const search_input = document.querySelector('.search_input');
 const search_btn = document.querySelector('.search_btn');
-let myXML;
 
 const base_url = 'http://api.data.go.kr/openapi/tn_pubr_public_weighted_envlp_api';
 //const api_key = 'V9hsM+3pd0KWaNCIJ82cLDqU1hJVBsaHI/nOwxsK1utzoE3d7HCHPYmKhD4zyUfoz2izB/9FiDJZFs6Hlt6b0g==';
 const api_key = 'V9hsM%2B3pd0KWaNCIJ82cLDqU1hJVBsaHI%2FnOwxsK1utzoE3d7HCHPYmKhD4zyUfoz2izB%2F9FiDJZFs6Hlt6b0g%3D%3D';
-let url;
 
-let no = 0;
+let myXML;
 
 // 공공데이터 fetch
 function fetchResult(evt) {
-  let pageNo = `${encodeURIComponent('pageNo')}=${encodeURIComponent(no++)}`;
-  url = `${base_url}?ServiceKey=${api_key}&${pageNo}`;
-  console.log(url);
+  let numOfRows = `${encodeURIComponent('numOfRows')}=${encodeURIComponent('1000')}`;
+  let pageNo = `${encodeURIComponent('pageNo')}=${encodeURIComponent('1')}`;
+  let url = `${base_url}?ServiceKey=${api_key}&${pageNo}&${numOfRows}`;
+  
   fetch(url).then(function(result) {
         return result.text();
      }).then(function(xml) {
@@ -31,8 +30,8 @@ function strToXML(xml) {
 }
 
 // 시군구 검색
-function searchCity() {
-    let searched_city = search.value;
+function search() {
+    let searched_city = search_input.value;
     
     display(searched_city);
 }
@@ -40,16 +39,20 @@ function searchCity() {
 // 종량제 봉투 정보 화면에 출력
 function display(searched_city) {
   console.log(myXML);
-  
+  while(para.firstChild) {
+    para.removeChild(para.firstChild);
+  }
+
   let item = myXML.getElementsByTagName('item');
 
   // 도시이름 검색
   for(let i=0; i<item.length; i++) {
     let city = item[i].getElementsByTagName('signguNm')[0].textContent;
+    console.log(city);
     if(searched_city === city) {
       h2.textContent = city;
 
-      let price = item[0].getElementsByTagName('price1')[0];
+      let price = item[i].getElementsByTagName('price1')[0];
 
       /// NODE.nextSibling 으로 가격 순회
       while(price.nodeName != 'chrgDeptNm') {
@@ -72,17 +75,9 @@ function display(searched_city) {
 
         price = price.nextSibling;
       }
-      no = 0;
       return
     }
   }
-
-  // page수 증가시켜서 다시 검색
-  if (no < 9) {
-    fetchResult();
-    searchCity();
-  }
-  
   // 해당 시군구가 없을 경우
   h2.textContent = '옳바른 도시이름을 입력 해주세요.';
   para.textContent = '';
@@ -90,7 +85,7 @@ function display(searched_city) {
 
 function init() {
   fetchResult();
-  search_btn.addEventListener('click', searchCity);
+  search_btn.addEventListener('click', search);
 }
 
 init();
